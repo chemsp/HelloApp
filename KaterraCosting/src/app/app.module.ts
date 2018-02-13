56732918c904ba7a8f28a43b12308e449ca28805
Sunil <chemsp@gmail.com>
2018-02-13 15:33:37 -0500

    add column feature added

--- KaterraCosting/src/app/app.module.ts
+++ KaterraCosting/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app.routing';
 import { HttpModule } from '@angular/http';
 import {TableModule} from 'primeng/table';
+import {MenuModule} from 'primeng/menu';
+import {TieredMenuModule} from 'primeng/tieredmenu';
+import {SharedModule, DataTableModule} from 'primeng/primeng';
+import {EditorModule} from 'primeng/editor';
 
+import {ContextMenuModule, ContextMenu} from 'primeng/contextmenu';
 
 import {TreeTableModule} from 'primeng/treetable';
 import {TreeNode} from 'primeng/api';
@@ -13,16 +19,23 @@ import {TreeNode} from 'primeng/api';
 import { AppComponent } from './app.component';
 import { TreeTableDemoComponent } from './tree-table-demo/tree-table-demo.component';
 import { NodeService } from './node.service';
+import { FilterPipeModule } from 'ngx-filter-pipe';
+
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TreeTableDemoComponent
+    TreeTableDemoComponent,
   ],
   imports: [
-    BrowserModule, AppRoutingModule, TreeTableModule, HttpModule, TableModule,
+    BrowserModule, AppRoutingModule, TreeTableModule, HttpModule, TableModule, ContextMenuModule, MenuModule,
+    TieredMenuModule, SharedModule, DataTableModule, FormsModule, CommonModule, EditorModule, FilterPipeModule,
+     ConfirmDialogModule
   ],
-  providers: [NodeService],
+  providers: [NodeService, ConfirmationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- KaterraCosting/src/app/tree-table-demo/tree-table-demo.component.ts
+++ KaterraCosting/src/app/tree-table-demo/tree-table-demo.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+
 import {TreeTableModule} from 'primeng/treetable';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
+
+import {ContextMenuModule} from 'primeng/contextmenu';
 import {TreeNode } from 'primeng/api';
 import { NodeService } from '../node.service';
 import {Http} from '@angular/http';
+import {SharedModule, DataTableModule} from 'primeng/primeng';
+import {ConfirmationService} from 'primeng/api';
 
 @Component({
   templateUrl: './tree-table-demo.component.html',
@@ -10,26 +17,145 @@ import {Http} from '@angular/http';
   providers: [NodeService],
 })
 export class TreeTableDemoComponent implements OnInit {
+    msgs = [];
+    selectedFiles: TreeNode[];
+    items: MenuItem[];
+    datastring = 'name';
+    showdata = false ;
+    text: string ;
+  private files: TreeNode[]  =  [
+        {
+            "data":{  
+                "name":"Documents",
+                "size":"75kb",
+                "type":"Folder"
+            },
+            "children":[
+                {  
+                    "data":{  
+                        "name":"Work",
+                        "size":"55kb",
+                        "type":"Folder"
+                    },
+                    "children":[  
+                        {  
+                            "data":{  
+                                "name":"Expenses.doc",
+                                "size":"30kb",
+                                "type":"Document"
+                            }
+                        },
+                        {  
+                            "data":{  
+                                "name":"Resume.doc",
+                                "size":"25kb",
+                                "type":"Resume"
+                            }
+                        }
+                    ]
+                },
+                {  
+                    "data":{  
+                        "name":"Home",
+                        "size":"20kb",
+                        "type":"Folder"
+                    },
+                    "children":[  
+                        {  
+                            "data":{  
+                                "name":"Invoices",
+                                "size":"20kb",
+                                "type":"Text"
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {  
+            "data":{  
+                "name":"Pictures",
+                "size":"150kb",
+                "type":"Folder"
+            },
+            "children":[  
+                {  
+                    "data":{  
+                        "name":"barcelona.jpg",
+                        "size":"90kb",
+                        "type":"Picture"
+                    }
+                },
+                {  
+                    "data":{  
+                        "name":"primeui.png",
+                        "size":"30kb",
+                        "type":"Picture"
+                    }
+                },
+                {  
+                    "data":{  
+                        "name":"optimus.jpg",
+                        "size":"30kb",
+                        "type":"Picture"
+                    }
+                }
+            ]
+        }
+    ]
+;
+  cols: any [];
+  cols1: any [];
 
-  files: TreeNode[];
-  cols: any[];
-  constructor(private nodeService: NodeService) {}
-
+  constructor(private nodeService: NodeService, private confirmationService: ConfirmationService) {
+  }
   ngOnInit() {
      this.nodeService.getFilesystem().then(files => this.files = files);
+     this.cols = Object.keys(this.files[0].data);
+     this.cols1  = Object.keys(this.files[0].data);
 
-     this.cols = [
-      { field: 'vin', header: 'Vin' },
-      { field: 'year', header: 'Year' },
-      { field: 'brand', header: 'Brand' },
-      { field: 'color', header: 'Color' }
-       ];
-           }
-
-addColumn(): void {
-
-  this.cols.push( { field: 'vin', header: 'Vin' });
-
+     this.items = [
+        {label: 'Add New Column', icon: 'fa-plus', command: (event) => this.addColumn('price')},
+        {label: 'Delete Column', icon: 'fa-close', command: (event) => this.removeColumn('price')}
+    ];
+    console.log(JSON.stringify(this.files));
 }
 
+addColumn(colname: string): void {
+
+                                    // for (const i of this.files) {
+                                    //     i.data[colname] = 'abc';
+                                    //     for ( const k of i.children) {
+                                    //                                     k.data[colname] = 'abc';
+                                    //                                 }
+                                    //                         }
+                                                            this.cols.push(colname);
+                                                            console.log(JSON.stringify(this.files));
+                                                        }
+ removeColumn(colname: string): void {
+   this.cols.splice( this.cols.indexOf(colname), 1);
+   console.log(JSON.stringify(this.files));
+ }
+
+
+nodeSelect(event) {
+    this.msgs = [];
+    this.msgs.push({severity: 'info', summary: 'Node Selected', detail: event.node.size});
+}
+
+nodeUnselect(event) {
+    this.msgs = [];
+    this.msgs.push({severity: 'info', summary: 'Node Unselected', detail: event.node.data.name});
+}
+
+
+
+confirm() {
+    this.confirmationService.confirm({
+        message: 'Are you sure that you want to perform this action?',
+        accept: () => {
+            //Actual logic to perform a confirmation
+        }
+    });
+}
 }
